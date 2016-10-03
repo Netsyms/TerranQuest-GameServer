1c5483b3c2c861e0a5d1762c681fdcb43a384a5e
Skylar Ittner <admin@netsyms.com>
2016-10-03 17:01:22 -0600

    getterrain.php output variable names now same as GIS API output. (issue #3)

--- getterrain.php
+++ getterrain.php
@@ -4,17 +4,17 @@ require 'required.php';
 require 'onlyloggedin.php';
 require 'latlong_validate.php';
 
-$terrain = json_decode(file_get_contents("http://gis.terranquest.net/terrain.php?key=" . GIS_API_KEY . "&lat=" . $VARS['lat'] . "&long=" . $VARS['long']));
+$terrain = json_decode(file_get_contents("http://gis.terranquest.net/terrain.php?key=" . GIS_API_KEY . "&lat=" . $VARS['lat'] . "&long=" . $VARS['long']), TRUE);
 
 if (!is_empty($terrain['error'])) {
     sendError($terrain['error'], true);
 } else {
     $out = [
         "status" => "OK",
-        "typeid" => $terrain['type'],
+        "type" => $terrain['type'],
         "latitude" => $terrain['latitude'],
         "longitude" => $terrain['longitude'],
-        "typename" => $terrain['name']
+        "name" => $terrain['name']
     ];
     die(json_encode($out));
 }
