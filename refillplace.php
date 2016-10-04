2b2df9947d471b945793cca6af68c6a9b37f9890
Skylar Ittner <admin@netsyms.com>
2016-10-03 22:37:25 -0600

    Add place refill code

---
+++ refillplace.php
@@ -0,0 +1,52 @@
+<?php
+
+require 'required.php';
+require 'onlyloggedin.php';
+
+if (is_empty($VARS['locationid'])) {
+    sendError("No target!", true);
+}
+
+$place = $database->select('locations', ['locationid', 'teamid', 'owneruuid', 'currentlife', 'maxlife'], ['locationid' => $VARS['locationid']])[0];
+$user = $database->select('players', ['level', 'teamid', 'energy', 'maxenergy', 'latitude', 'longitude'], ['uuid' => $_SESSION['uuid']])[0];
+
+// This (probably) shouldn't happen in normal play
+if ($place['teamid'] != $user['teamid']) {
+    sendError("Wrong team!", true);
+}
+
+if ($place['currentlife'] == 100) {
+    sendError("Full!", true);
+}
+
+$userdrain = 5 * floor($user['level']);
+
+// Calculate resulting user HP
+$userhp = $user['energy'] - $userdrain;
+// Check if action possible
+if ($userhp < 0) {
+    sendError("No life!", true);
+}
+
+// Update the user's health and level
+$exp = pow(pow(floor($user['level']) + 1, 2), -1.2);
+$userlevel = $user['level'] + $exp;
+// If the new level is a whole int bigger than the current
+$dolevelup = false;
+if (floor($userlevel) > floor($user['level'])) {
+    $dolevelup = true;
+    $newmaxhp = floor($userlevel) * 100;
+    $database->update('players', ['energy' => $newmaxhp, 'maxenergy' => $newmaxhp, 'level' => $userlevel], ['uuid' => $_SESSION['uuid']]);
+} else {
+    $database->update('players', ['energy' => $userhp, 'level' => $userlevel], ['uuid' => $_SESSION['uuid']]);
+}
+
+$placelife = $place['currentlife'] + 10;
+if ($placelife > 100) {
+    $placelife = 100;
+}
+
+// Update the place
+$database->update('locations', ['currentlife' => $placelife, 'maxlife' => 100, 'owneruuid' => $_SESSION['uuid'], 'teamid' => $user['teamid']], ['locationid' => $VARS['locationid']]);
+
+sendOK(($dolevelup ? "Level up!" : "Refilled!"));
