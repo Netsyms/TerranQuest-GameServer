a8735c03aa0847dcf14cb3e3e93674425d2b38fd
Skylar Ittner <admin@netsyms.com>
2016-10-11 01:20:32 -0600

    Add giveitem.php

---
+++ giveitem.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'required.php';
+require 'onlyloggedin.php';
+
+$itemuuid = $VARS['itemuuid'];
+$player = $VARS['giveto'];
+
+if (is_empty($itemuuid) || !is_numeric($itemuuid) || !$database->has('inventory', ["AND" => ['itemuuid' => $itemuuid, 'playeruuid' => $_SESSION['uuid']]])) {
+    sendError("Invalid itemuuid.", true);
+}
+
+if (is_empty($player) || !$database->has('players', ['nickname' => $player])) {
+    sendError("Invalid nickname.", true);
+}
+
+$playeruuid = $database->select('players', ['uuid'], ['nickname' => $player])[0];
+
+
+$database->update('inventory', ['playeruuid' => $playeruuid], ['itemuuid' => $itemuuid]);
