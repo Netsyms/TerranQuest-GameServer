3e63bab9d758b68590aa33e9d161869533ecbf07
Skylar Ittner <admin@netsyms.com>
2017-02-23 23:26:47 -0700

    Add kick code to getstats.php

--- getstats.php
+++ getstats.php
@@ -8,8 +8,17 @@ if (is_empty($VARS['user'])) {
     sendError("Missing data.", true);
 }
 
-$stats = $database->select('players', ['level', 'energy', 'maxenergy', 'teamid', 'lastping'], ['nickname' => $VARS['user']])[0];
+$stats = $database->select('players', ['level', 'energy', 'maxenergy', 'teamid', 'lastping', 'kick'], ['nickname' => $VARS['user']])[0];
 
+$uuid = $_SESSION['uuid'];
+$kick = $database->select('players', ['kick'], ['uuid' => $uuid])[0]['kick'];
+if (!is_empty($kick)) {
+    $_SESSION['loggedin'] = false;
+    session_unset();
+    session_destroy();
+    $database->update('players', ['kick' => ''], ['uuid' => $uuid]);
+    die(json_encode(['status' => 'ERROR', 'kick' => 1, 'message' => $kick]));
+}
 
 $out = [];
 $out['status'] = 'OK';
